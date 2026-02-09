f5bec21427409f4a8a80e8152bd682a24cd3a205
Hristijan Ralevski <hristijanr@gecko.mk>
2026-02-09 11:24:52 +0100

    Created folder structure for better code organisation

---
+++ NationalWeather/src/app/common/city-weather/city-weather.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-weather',
+  imports: [],
+  templateUrl: './city-weather.html',
+  styleUrl: './city-weather.css',
+})
+export class CityWeather {
+
+}
---
+++ NationalWeather/src/app/layout/components/additional-info/additional-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-additional-info',
+  imports: [],
+  templateUrl: './additional-info.html',
+  styleUrl: './additional-info.css',
+})
+export class AdditionalInfo {
+
+}
