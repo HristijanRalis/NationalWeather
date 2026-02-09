c91857c83a8d08335edd7117de48396c93357cf7
Hristijan Ralevski <hristijanr@gecko.mk>
2026-02-09 11:37:30 +0100

    Created main-layout with header sidebar & additional-info

--- NationalWeather/src/app/common/city-weather/city-weather.ts
+++ NationalWeather/src/app/common/city-weather/city-weather.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Header } from '../../layout/components/header/header';
+import { Sidebar } from '../../layout/components/sidebar/sidebar';
+import { AdditionalInfo } from '../../layout/components/additional-info/additional-info';
 
 @Component({
   selector: 'app-city-weather',
-  imports: [],
+  imports: [Header, Sidebar, AdditionalInfo],
   templateUrl: './city-weather.html',
   styleUrl: './city-weather.css',
 })
-export class CityWeather {
-
-}
+export class CityWeather {}
