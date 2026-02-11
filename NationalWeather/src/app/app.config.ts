d0d6a48a98fe0a5b570a23f527d50765227799e1
Hristijan Ralevski <hristijanr@gecko.mk>
2026-02-11 11:54:24 +0100

    Lessons for adding API

--- NationalWeather/src/app/app.config.ts
+++ NationalWeather/src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     providePrimeNG({
       theme: {
         preset: Aura,
